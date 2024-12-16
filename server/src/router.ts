709992c66a50a2386dbebdda7ad56984f4bbbaec
FarCodeFR <Timo-44@hotmail.com>
2024-12-16 15:31:14 +0100

    quest-04-routing-avancé

--- server/src/router.ts
+++ server/src/router.ts
@@ -8,8 +8,9 @@ const router = express.Router();
 
 // Define item-related routes
 import itemActions from "./modules/item/itemActions";
-import sayActions from "./modules/say/sayActions";
 import programActions from "./modules/program/programActions";
+import sayActions from "./modules/say/sayActions";
+import categoryActions from "./modules/category/categoryActions";
 
 router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
@@ -21,5 +22,9 @@ router.post("/api/items", itemActions.add);
 
 router.get("/", sayActions.sayWelcome);
 router.get("/api/programs", programActions.browse);
+router.get("/api/programs/:id", programActions.read);
+
+router.get("/api/categories", categoryActions.browse);
+router.get("/api/categories/:id", categoryActions.read);
 
 export default router;
